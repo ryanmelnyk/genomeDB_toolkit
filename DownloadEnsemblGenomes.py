2d25573a3b49875847ad030d09969cdb71526007
Ryan A. Melnyk <schmelnyk@gmail.com>
2016-08-14 23:02:19 -0700

    added data extraction for JSON records. also github test

--- DownloadEnsemblGenomes.py
+++ DownloadEnsemblGenomes.py
@@ -23,15 +23,44 @@ def parse_json():
 
 	items = ijson.items(urlopen("ftp://ftp.ensemblgenomes.org/{}/species_metadata_EnsemblBacteria.json".format(ens.pwd())),'item')
 	count = 0
+	finished_genomes = {}
+	o = open("test.txt",'w')
 	for js in items:
 		count += 1
+
+		# block for getting JSON datatypes
+		# if count > 1: break
+		# for k in js:
+		# 	print k, js[k]
+
+		# print js['assembly_level']
+		# if js['assembly_level'] == "chromosome":
+
+		thisline = []
+		finished_genomes[js["assembly_id"]] = {}
+		thisline.append(js["assembly_id"])
+		for feat in ['assembly_level','base_count','name', 'strain', 'dbname','taxonomy_id']:
+			finished_genomes[js["assembly_id"]][feat] = js[feat]
+			thisline.append(js[feat])
+		finished_genomes[js["assembly_id"]]['contigs'] = len(js["sequences"])
+		thisline.append(str(len(js["sequences"])))
+		finished_genomes[js["assembly_id"]]['ngenes'] = js["annotations"]["nProteinCoding"]
+		thisline.append(js["annotations"]["nProteinCoding"])
+
+		o.write("\t".join([str(x) for x in thisline])+"\n")
+
+
 		if count % 100 == 0:
 			print count, "JSON records parsed."
 
+	print len(finished_genomes), "of these are finished genomes."
 
+
+	return finished_genomes
 
 def main():
-	parse_json()
+	finished_genomes = parse_json()
+
 
 
 if __name__ == '__main__':
