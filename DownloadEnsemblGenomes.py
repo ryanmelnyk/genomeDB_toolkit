391af07406b6340522c36a1095e522a2f03d1f7c
Ryan A. Melnyk <schmelnyk@gmail.com>
2016-09-09 17:56:25 -0700

    Revert "showing Whitney atom and git"
    This reverts commit e548772ef0597c71e6abc7ec0d83628c1da9d209.

--- DownloadEnsemblGenomes.py
+++ DownloadEnsemblGenomes.py
@@ -2,8 +2,6 @@
 #schmelnyk@gmail.com
 #UBC Microbiology - Haney Lab
 
-## blurb to show git
-
 # This script is used to download genomes from Ensembl Bacteria (http://bacteria.ensembl.org/)
 
 import ijson
