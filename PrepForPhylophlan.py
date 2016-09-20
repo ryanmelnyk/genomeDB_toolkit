656cfa5aeb7fc7eb3107695668767737fb36ce66
Ryan A. Melnyk <schmelnyk@gmail.com>
2016-09-19 22:32:37 -0700

    adding quick script to prep genomedb for Phylophlan

---
+++ PrepForPhylophlan.py
@@ -0,0 +1,17 @@
+#Ryan A. Melnyk
+#schmelnyk@gmail.com
+#UBC Microbiology - Haney Lab
+
+def parse_args():
+	parser = argparse.ArgumentParser(description='''
+Organize files for
+	''')
+	parser.add_argument('path_to_genomedb', type=,help='relative path to genomedb set up by DownloadEnsemblGenomes.py')
+	parser.add_argument('path_to_phylophlan', type=,help='relative path to phylophlan directory')
+	return parser.parse_args()
+
+def main():
+	pass
+
+if __name__ == '__main__':
+	main()
