c103b5aa0c3aab743f339baf6a157a039aebfa31
Ryan A. Melnyk <schmelnyk@gmail.com>
2016-09-26 12:43:06 -0700

    wrote GetTaxData.py

---
+++ GetTaxData.py
@@ -0,0 +1,83 @@
+#Ryan A. Melnyk
+#schmelnyk@gmail.com
+#UBC Microbiology - Haney Lab
+
+import argparse, psycopg2
+from Bio import Entrez
+import datetime
+
+
+def parse_args():
+	parser = argparse.ArgumentParser(description='''
+Populate the taxonomy psql table with taxonomy information extracted from NCBI Taxonomy.
+	''')
+	parser.add_argument('sql_user',type=str, help='user for sql')
+	parser.add_argument('sql_db',type=str,help='name of sql database')
+	parser.add_argument('sql_host', type=str, help='host for sql: "localhost" for MacOSX psql install, "172.18.0.71" for bugaboo')
+	return parser.parse_args()
+
+def get_genomedb_data(sql_user,sql_db,sql_host):
+
+	con = psycopg2.connect(user=sql_user, dbname=sql_db, host=sql_host, password='')
+	cur = con.cursor()
+	cur.execute("SELECT * FROM genome_metadata")
+	records = cur.fetchall()
+	taxdata = {}
+	for r in records:
+		taxdata[r[4]] = r[5]
+
+	cur.close()
+	con.close()
+	return taxdata
+
+def fetch_tax(taxdata,sql_user,sql_db,sql_host):
+	con = psycopg2.connect(user=sql_user, dbname=sql_db, host=sql_host, password='')
+	cur = con.cursor()
+	Entrez.email = "schmelnyk@gmail.com"
+	sql = """INSERT INTO taxonomy (species_id, taxonomy_id, superkingdom, phylum, class,
+			__order__, family, genus, species, date_added, date_modified) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s);"""
+	count = len(taxdata)
+	print "Extracting", str(count), "taxonomy records from Entrez-NCBI..."
+	for t in taxdata:
+		values = [t,taxdata[t],None,None,None,None,None,None,None,datetime.datetime.now(),datetime.datetime.now()]
+		records = Entrez.parse(Entrez.efetch(db="taxonomy",id=taxdata[t],retmode="xml"))
+		for r in records:
+			for l in r["LineageEx"]:
+				if l["Rank"] == "superkingdom":
+					values[2] = l["ScientificName"]
+				elif l["Rank"] == "phylum":
+					values[3] = l["ScientificName"]
+				elif l["Rank"] == "class":
+					values[4] = l["ScientificName"]
+				elif l["Rank"] == "order":
+					values[5] = l["ScientificName"]
+				elif l["Rank"] == "genus":
+					values[6] = l["ScientificName"]
+				elif l["Rank"] == "species":
+					values[7] = l["ScientificName"]
+				else:
+					pass
+		count -= 1
+		if count % 100 == 0:
+			print count, "records remaining..."
+
+		cur.execute(sql,values)
+		con.commit()
+	cur.close()
+	con.close()
+	print "Done!"
+
+	return
+
+
+def main():
+	args = parse_args()
+	sql_user = args.sql_user
+	sql_db = args.sql_db
+	sql_host = args.sql_host
+
+	taxdata = get_genomedb_data(sql_user,sql_db,sql_host)
+	fetch_tax(taxdata,sql_user,sql_db,sql_host)
+
+if __name__ == '__main__':
+	main()
