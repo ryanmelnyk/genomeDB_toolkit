f4bcd5ac74b39feb8d5396589e69b743f0662327
Ryan A. Melnyk <schmelnyk@gmail.com>
2017-06-03 15:47:25 -0700

    add the --max option and try-except entire ftp transfer section

--- DownloadEnsemblGenomes.py
+++ DownloadEnsemblGenomes.py
@@ -22,9 +22,10 @@ well as a metadata table for SQL.
 	parser.add_argument('sql_db',type=str,help='name of sql database')
 	parser.add_argument('sql_host', type=str, help='host for sql: "localhost" for MacOSX psql install, "172.18.0.71" for bugaboo')
 	parser.add_argument('--names', type=str, help='comma-separated list of keywords to find in name of strain to download draft genomes. i.e. "pseudomonas,salmonella,syringae,K12". Only complete/exact matches will be downloaded and spelling counts!')
+	parser.add_argument('--max', type=int, help="maximum number of genomes to download")
 	return parser.parse_args()
 
-def parse_json(outdir, assemblies, names):
+def parse_json(outdir, assemblies, names, maxgen):
 	ens = ftplib.FTP('ftp.ensemblgenomes.org')
 	ens.login()
 	ens.cwd('pub/bacteria/current')
@@ -46,6 +47,7 @@ def parse_json(outdir, assemblies, names):
 	count = 0
 	finished_genomes = {}
 
+	found = 0
 	for js in items:
 		count += 1
 
@@ -57,27 +59,35 @@ def parse_json(outdir, assemblies, names):
 		thisline.append(js["annotations"]["nProteinCoding"])
 
 		if js["assembly_level"] == "chromosome":
-			if js["assembly_id"] not in assemblies:
+			if js["species"] not in assemblies:
+				# print js["species"]
 				finished_genomes[js["assembly_id"]] = {}
 				for feat in ['assembly_level','base_count','name', 'strain', 'dbname','species','taxonomy_id']:
 					finished_genomes[js["assembly_id"]][feat] = js[feat]
 				finished_genomes[js["assembly_id"]]['contigs'] = len(js["sequences"])
 				finished_genomes[js["assembly_id"]]['ngenes'] = js["annotations"]["nProteinCoding"]
+				found +=1
 		else:
 			fields = js['species'].split("_")
 			for n in names:
 				if n in fields:
-					if js["assembly_id"] not in assemblies:
+					if js["species"] not in assemblies:
+						# print js["species"]
 						finished_genomes[js["assembly_id"]] = {}
 						for feat in ['assembly_level','base_count','name', 'strain', 'dbname','species','taxonomy_id']:
 							finished_genomes[js["assembly_id"]][feat] = js[feat]
 						finished_genomes[js["assembly_id"]]['contigs'] = len(js["sequences"])
 						finished_genomes[js["assembly_id"]]['ngenes'] = js["annotations"]["nProteinCoding"]
+						found += 1
 
 		o.write("\t".join([str(x) for x in thisline])+"\n")
 
-		if count % 500 == 0:
+		if count % 1000 == 0:
 			print count, "JSON records parsed."
+		if maxgen is not None:
+			if found == maxgen:
+				print "{} new genomes to download found...exiting JSON parser...".format(found)
+				break
 
 	print count, "total JSON records parsed."
 	print len(assemblies), "found in", os.path.basename(outdir)+"."
@@ -110,31 +120,35 @@ def get_files(fg, outdir, EV, sql_user, sql_db, sql_host):
 	print "Downloading genome files..."
 	count = 0
 	for f in fg:
-		ens.cwd("/pub/bacteria/current/fasta/{}/{}/dna".format("_".join(fg[f]["dbname"].split("_")[0:3]),fg[f]["species"]))
-		for filepath in ens.nlst():
-			if filepath.endswith(".dna.toplevel.fa.gz"):
-				download_and_unzip(ens,filepath,os.path.join(outdir,"dna",fg[f]["species"]+".dna.fa.gz"))
-		ens.cwd("../pep")
-		for filepath in ens.nlst():
-			if filepath.endswith(".pep.all.fa.gz"):
-				download_and_unzip(ens,filepath,os.path.join(outdir,"pep",fg[f]["species"]+".pep.fa.gz"))
-		ens.cwd("/pub/bacteria/current/gff3/{}/{}".format("_".join(fg[f]["dbname"].split("_")[0:3]),fg[f]["species"]))
-		for filepath in ens.nlst():
-			fields = filepath.split(".")
-			if ".".join(fields[3:]) == ("gff3.gz"):
-				download_and_unzip(ens,filepath,os.path.join(outdir,"gff3",fg[f]["species"]+".gff3.gz"))
-		sql = """INSERT INTO genome_metadata (assembly_id, base_count, species, taxonomy_id, contigs,
-			protein_coding_genes, source, date_added, date_modified) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);"""
-		vals = [f]
-		for key in ["base_count", "species", "taxonomy_id", "contigs","ngenes"]:
-			vals.append(fg[f][key])
-		vals.append("ensembl-"+EV)
-		vals.append(datetime.datetime.now())
-		vals.append(datetime.datetime.now())
-		cur.execute(sql, vals)
-		con.commit()
-		count += 1
-		print fg[f]["species"], "processed.", count, "files downloaded."
+		try:
+			ens.cwd("/pub/bacteria/current/fasta/{}/{}/dna".format("_".join(fg[f]["dbname"].split("_")[0:3]),fg[f]["species"]))
+			for filepath in ens.nlst():
+				if filepath.endswith(".dna.toplevel.fa.gz"):
+					download_and_unzip(ens,filepath,os.path.join(outdir,"dna",fg[f]["species"]+".dna.fa.gz"))
+			ens.cwd("../pep")
+			for filepath in ens.nlst():
+				if filepath.endswith(".pep.all.fa.gz"):
+					download_and_unzip(ens,filepath,os.path.join(outdir,"pep",fg[f]["species"]+".pep.fa.gz"))
+			ens.cwd("/pub/bacteria/current/gff3/{}/{}".format("_".join(fg[f]["dbname"].split("_")[0:3]),fg[f]["species"]))
+			for filepath in ens.nlst():
+				fields = filepath.split(".")
+				if ".".join(fields[3:]) == ("gff3.gz"):
+					download_and_unzip(ens,filepath,os.path.join(outdir,"gff3",fg[f]["species"]+".gff3.gz"))
+			sql = """INSERT INTO genome_metadata (assembly_id, base_count, species, taxonomy_id, contigs,
+				protein_coding_genes, source, date_added, date_modified) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s);"""
+			vals = [f]
+			for key in ["base_count", "species", "taxonomy_id", "contigs","ngenes"]:
+				vals.append(fg[f][key])
+			vals.append("ensembl-"+EV)
+			vals.append(datetime.datetime.now())
+			vals.append(datetime.datetime.now())
+			cur.execute(sql, vals)
+			con.commit()
+			count += 1
+			print fg[f]["species"], "processed.", count, "files downloaded."
+		except ftplib.error_perm:
+			print "Issue with data for {}...skipping.".format(f)
+
 
 	ens.close()
 	cur.close()
@@ -157,7 +171,7 @@ def query_sql(sql_user,sql_db,sql_host):
 	records = cur.fetchall()
 	assemblies = []
 	for r in records:
-		assemblies.append(r[1])
+		assemblies.append(r[4])
 
 	cur.close()
 	con.close()
@@ -198,7 +212,7 @@ def main():
 
 	assemblies = query_sql(sql_user,sql_db,sql_host)
 
-	finished_genomes, ENSEMBL_VERSION = parse_json(outdir,assemblies,names)
+	finished_genomes, ENSEMBL_VERSION = parse_json(outdir,assemblies,names,args.max)
 	get_files(finished_genomes, outdir, ENSEMBL_VERSION, sql_user, sql_db, sql_host)
 	dump_flat_file(outdir,sql_user,sql_db,sql_host)
 
